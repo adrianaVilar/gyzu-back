3d40c20ab9d05730df6ddc9dae237277afbfbd45
adrianaVilar <adriana.correa@outlook.com>
2022-06-14 14:20:57 -0300

    Requisição utilizando id

--- Requisicao/assets/js/main.js
+++ Requisicao/assets/js/main.js
@@ -2,15 +2,17 @@ fetch("https://jsonplaceholder.typicode.com/posts/")
   .then((response) => response.json())
   .then((json) => carregaPostsNaPagina(json));
 
+const filtroPorUsuario = (obj, id) => obj.userId === id;
+
 function carregaPostsNaPagina(json) {
   const div = document.createElement("div");
 
   const jsonFiltrado = json.filter(function (obj) {
-    return obj.userId === 1;
+    return filtroPorUsuario(obj, 1);
   });
 
   for (let posts of jsonFiltrado) {
-    console.log(posts);
+    // console.log(posts);
 
     // div.innerHTML += `<p> ${Object.values(posts)}`;
     div.innerHTML += `<p>${JSON.stringify(posts)}`;
