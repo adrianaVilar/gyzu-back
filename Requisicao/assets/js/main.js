00bcb49678797094d356ac6082e82db771ccff89
adrianaVilar <adriana.correa@outlook.com>
2022-06-14 20:49:34 -0300

    Requisição atualizado css e template

--- Requisicao/assets/js/main.js
+++ Requisicao/assets/js/main.js
@@ -2,22 +2,32 @@ fetch("https://jsonplaceholder.typicode.com/posts/")
   .then((response) => response.json())
   .then((json) => carregaPostsNaPagina(json));
 
-const filtroPorUsuario = (obj, id) => obj.userId === id;
+let filtroPorUsuario = (obj, id) => obj.userId === id;
 
 function carregaPostsNaPagina(json) {
-  const div = document.createElement("div");
+  // let i = 1;
+  // const newLocal = "click";
+  // document.addEventListener(newLocal, (e) => {
+  //   const el = e.target;
+  //   e.preventDefault();
+  //   if (el.classList.contains("avancar")) i++;
+  //   if (el.classList.contains("retornar")) i--;
+  //   // console.log(`${i}`);
+  // });
 
-  const jsonFiltrado = json.filter(function (obj) {
+  let jsonFiltrado = json.filter(function (obj) {
     return filtroPorUsuario(obj, 1);
   });
 
-  for (let posts of jsonFiltrado) {
-    // console.log(posts);
-
-    // div.innerHTML += `<p> ${Object.values(posts)}`;
-    div.innerHTML += `<p>${JSON.stringify(posts)}`;
+  function template(post) {
+    return `
+    <article class="post">
+    <p> <b>${post.id}) Usuário:</b> ${post.userId} </p>
+    <p><b> Título:</b> ${post.title} </p>
+    <p> <b>Post: </b>${post.body} </p>
+    </article>`;
   }
 
   const resultado = document.querySelector(".resultado");
-  resultado.appendChild(div);
+  resultado.innerHTML += `${jsonFiltrado.map(template).join("")}`;
 }
