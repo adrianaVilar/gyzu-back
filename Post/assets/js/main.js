1624949b2eea003ac492e98c0d94f208d66f23e2
adrianaVilar <adriana.correa@outlook.com>
2022-06-15 02:18:20 -0300

    Tentativa de postar e apagar

--- Post/assets/js/main.js
+++ Post/assets/js/main.js
@@ -1,33 +1,65 @@
-fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+fetch("https://jsonplaceholder.typicode.com/posts/")
   .then((response) => response.json())
   .then((json) => carregaPostsNaPagina(json));
 
 let filtroPorUsuario = (obj, id) => obj.userId === id;
+let post;
 
 function carregaPostsNaPagina(json) {
-  // let i = 1;
-  // const newLocal = "click";
-  // document.addEventListener(newLocal, (e) => {
-  //   const el = e.target;
-  //   e.preventDefault();
-  //   if (el.classList.contains("avancar")) i++;
-  //   if (el.classList.contains("retornar")) i--;
-  //   // console.log(`${i}`);
-  // });
-
   let jsonFiltrado = json.filter(function (obj) {
     return filtroPorUsuario(obj, 1);
   });
 
-  function template(post) {
-    return `
-    <article class="post">
-    <p> <b>${post.id}) Usuário:</b> ${post.userId} </p>
-    <p><b> Título:</b> ${post.title} </p>
-    <p> <b>Post: </b>${post.body} </p>
-    </article>`;
-  }
-
-  const resultado = document.querySelector(".resultado");
-  resultado.innerHTML += `${jsonFiltrado.map(template).join("")}`;
+  const resultadoFiltro = document.querySelector(".resultadoFiltro");
+  resultadoFiltro.innerHTML += `${jsonFiltrado.map(template).join("")}`;
 }
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Post 1",
+    body: "Post criado para a resolução do exercício de html Squad RedBull",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((jsonCriado) => getJson(jsonCriado));
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Post 2",
+    body: "Segundo post criado para a resolução do exercício de html Squad RedBull",
+    userId: 11,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((jsonCriado) => getJson(jsonCriado));
+
+let postCriado = (jsonCriado, id) => jsonCriado === id;
+
+let pegaPost = function getJson(jsonCriado) {
+  return postCriado(jsonCriado, 1);
+};
+
+const resultadoPost = document.querySelector(".resultadoPost");
+resultadoPost.innerHTML += `${pegaPost.map(template).join("")}`;
+
+function template(post) {
+  return `
+      <article class="post">
+      <p><b>${post.id}) Usuário:</b> ${post.userId} </p>
+      <p><b> Título:</b> ${post.title} </p>
+      <p> <b>Post: </b>${post.body} </p>
+      </article>`;
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "DELETE",
+});
