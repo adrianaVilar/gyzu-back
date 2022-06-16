efccf0fde21936c0c4779ff571144696b0230c25
adrianaVilar <adriana.correa@outlook.com>
2022-06-16 00:39:02 -0300

    Post e delete

--- Post/assets/js/main.js
+++ Post/assets/js/main.js
@@ -1,55 +1,4 @@
-fetch("https://jsonplaceholder.typicode.com/posts/")
-  .then((response) => response.json())
-  .then((json) => carregaPostsNaPagina(json));
-
-let filtroPorUsuario = (obj, id) => obj.userId === id;
-let post;
-
-function carregaPostsNaPagina(json) {
-  let jsonFiltrado = json.filter(function (obj) {
-    return filtroPorUsuario(obj, 1);
-  });
-
-  const resultadoFiltro = document.querySelector(".resultadoFiltro");
-  resultadoFiltro.innerHTML += `${jsonFiltrado.map(template).join("")}`;
-}
-
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify({
-    title: "Post 1",
-    body: "Post criado para a resolução do exercício de html Squad RedBull",
-    userId: 1,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((jsonCriado) => getJson(jsonCriado));
-
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify({
-    title: "Post 2",
-    body: "Segundo post criado para a resolução do exercício de html Squad RedBull",
-    userId: 11,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((jsonCriado) => getJson(jsonCriado));
-
-let postCriado = (jsonCriado, id) => jsonCriado === id;
-
-let pegaPost = function getJson(jsonCriado) {
-  return postCriado(jsonCriado, 1);
-};
-
-const resultadoPost = document.querySelector(".resultadoPost");
-resultadoPost.innerHTML += `${pegaPost.map(template).join("")}`;
+let posts = [];
 
 function template(post) {
   return `
@@ -60,6 +9,70 @@ function template(post) {
       </article>`;
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: "DELETE",
+function buscaPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/")
+    .then((response) => response.json())
+    .then((json) => {
+      posts.push(...json);
+      console.log("Busca: ", posts);
+      return carregaPostsNaPagina(posts);
+    });
+}
+
+function carregaPostsNaPagina(posts) {
+  let jsonFiltrado = posts.filter(function (obj) {
+    return filtroPorUsuarioOuIdMaiorQueCem(obj, 1);
+  });
+
+  const resultadoFiltro = document.querySelector(".resultadoFiltro");
+  resultadoFiltro.innerHTML = `${jsonFiltrado.map(template).join("")}`;
+}
+
+let filtroPorUsuarioOuIdMaiorQueCem = (obj, userId) =>
+  obj.userId === userId || obj.id > 100;
+
+function criaPost(post) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(post),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((jsonCriado) => posts.push(jsonCriado))
+    .then((jsonCriado) => console.log("Cria post indice: ", jsonCriado))
+    .then(() => carregaPostsNaPagina(posts));
+}
+
+function deletePost(indiceParaDeletar) {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${indiceParaDeletar}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((json) => console.log("Deletado: ", json))
+    .then(() => {
+      let idDoPosts = posts.map((obj) => obj.id);
+      indiceParaDeletar = idDoPosts.indexOf(indiceParaDeletar);
+      if (indiceParaDeletar > -1) posts.splice(indiceParaDeletar, 1);
+    })
+    .then(() => console.log(posts));
+}
+
+buscaPost();
+
+criaPost({
+  id: 101,
+  title: "Post 2",
+  body: "Segundo post criado para a resolução do exercício de html Squad RedBull",
+  userId: 11,
 });
+
+criaPost({
+  id: 102,
+  title: "Post 1",
+  body: "Post criado para a resolução do exercício de html Squad RedBull",
+  userId: 1,
+});
+
+deletePost(101);
